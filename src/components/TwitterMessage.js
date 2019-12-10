e6e5056e1dd2c66606a6a3f976370f23c2dfb20d
github username <github email address>
2019-12-10 18:48:04 -0500

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,32 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
 
-    this.state = {};
+
+  constructor(props) {
+    super();
+    this.state = {
+      message: "",
+      newChar: props.maxChars
+    };
+  }
+
+  handleChange = event => {
+    //debugger
+    let nChar = this.props.maxChars - event.target.value.length
+    this.setState({
+      message: event.target.value,
+      newChar: nChar
+    })
+
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.state.message} onChange={event => this.handleChange(event)} />
+        <p>{this.state.newChar} left</p>
       </div>
     );
   }
